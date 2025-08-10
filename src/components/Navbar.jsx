e9275960651510f9efda005fb4434a5cd495cbe9
LaboneeAktar <labonipub@gmail.com>
2025-08-10 18:28:32 +0600

    Created a navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,25 @@
+import navbarBG from "../assets/navBg.png";
+import logo from "../assets/Logo.png";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <div>
-      <h5>This is Aroma navbar</h5>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
+    <div
+      style={{
+        backgroundImage: `url(${navbarBG})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <span className="flex justify-center items-center gap-3 py-2">
+        <img className="h-14 w-10" src={logo} alt="" />
+        <Link to="/">
+          {" "}
+          <h2 className="text-4xl text-white font-dancing">
+            Aroma Caffeine
+          </h2>{" "}
+        </Link>
+      </span>
     </div>
   );
 };

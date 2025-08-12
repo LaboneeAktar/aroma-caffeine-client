7cd302c1e4b3f91f0ae36e58a660a36bcae63b45
LaboneeAktar <labonipub@gmail.com>
2025-08-12 16:06:05 +0600

    Handle Updated Coffee

--- src/Pages/UpdateCoffee.jsx
+++ src/Pages/UpdateCoffee.jsx
@@ -1,11 +1,69 @@
 import { FaArrowLeft } from "react-icons/fa";
 import bgImage from "../assets/CoffeeBG.png";
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const UpdateCoffee = () => {
   const coffee = useLoaderData();
-  const { name, chef, supplier, taste, category, details, price, photo } =
+  const { _id, name, chef, supplier, taste, category, details, price, photo } =
     coffee;
+
+  const navigate = useNavigate();
+
+  //Update Coffee data
+  const handleUpdateCoffee = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const name = form.name.value;
+    const chef = form.chef.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const price = form.price.value;
+    const photo = form.photo.value;
+
+    const updateCoffee = {
+      name,
+      chef,
+      supplier,
+      taste,
+      category,
+      details,
+      price,
+      photo,
+    };
+
+    //send data to the server
+    fetch(`http://localhost:5000/coffee/${_id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updateCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        if (data.modifiedCount > 0) {
+          Swal.fire({
+            title: "Good job!",
+            text: "Successfully Updated Your Coffee!",
+            icon: "success",
+            iconColor: "#023020",
+            confirmButtonColor: "#331a15",
+            color: "#331a15",
+            background: "#d2b48c",
+            width: 400,
+          });
+          form.reset();
+          navigate("/");
+        }
+      })
+      .catch((error) => console.error(error));
+  };
 
   return (
     <div
@@ -40,7 +98,7 @@ const UpdateCoffee = () => {
             </p>
           </div>
           <form
-            // onSubmit={handleAddCoffee}
+            onSubmit={handleUpdateCoffee}
             className="w-10/12 mx-auto pt-4 pb-14"
           >
             <div className="flex flex-wrap -mx-3 mb-6">
